50968dc668b040534f9a1cf948c810a8a24bd1e6
WedroNadzor <demik1995@yandex.ua>
2017-01-19 22:12:53 +0200

    Update TitlesFrame.java

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -9,21 +9,27 @@ public class TitlesFrame
 extends JFrame {
     public TitlesFrame() {
         this.initUI();
-    }
+    }   
 
     private void initUI() {
+        /** Відповідає за вивід вікна з рисунком
+        * @param setSize-ромір вікна
+        */
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
         this.add(new TitlesPanel(94));
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
-
+    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable(){
-
-            @Override
+            
+        
+               @Override
             public void run() {
+                /** Викликає класс TitlesFrame зупска програми 
+                 */
                 TitlesFrame ps = new TitlesFrame();
                 ps.setVisible(true);
             }
